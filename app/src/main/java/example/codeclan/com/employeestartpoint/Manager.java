c5368be40ebf1d0abbc6cb8eaade468bac7e05a6
John Duncan <john@johnduncanphoto.com>
2017-10-31 20:04:00 +0000

    manager class constructor created and depName property added

--- app/src/main/java/example/codeclan/com/employeestartpoint/Manager.java
+++ app/src/main/java/example/codeclan/com/employeestartpoint/Manager.java
@@ -4,7 +4,36 @@ package example.codeclan.com.employeestartpoint;
  * Created by JohnD on 31/10/2017.
  */
 
-//public class Manager extends Employee {
-//
-//
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager (String deptName, int id, String name, String socialSecurityNumber, double salary) {
+        super(id, name, socialSecurityNumber, salary);
+        this.deptName = deptName;
+    }
+}
+
+
+
+
+
+
+//    public Manager() {
+//        super(int, String, String, double);
+//    }
 //}
+
+/*
+private int id;
+private String name;
+private String socialSecurityNumber;
+private double salary;
+
+public Manager {
+this.id = id;
+this.name = name;
+this.socialSecurityNumber = socialSecurityNumber;
+this.salary = salary;
+}
+*/
