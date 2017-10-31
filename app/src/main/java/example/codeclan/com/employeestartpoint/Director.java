73e4da3dfe54cd445c951da242c970aa2843840e
John Duncan <john@johnduncanphoto.com>
2017-10-31 20:34:51 +0000

    Director class set up with apropriate constructors, extends manager

--- app/src/main/java/example/codeclan/com/employeestartpoint/Director.java
+++ app/src/main/java/example/codeclan/com/employeestartpoint/Director.java
@@ -4,5 +4,12 @@ package example.codeclan.com.employeestartpoint;
  * Created by JohnD on 31/10/2017.
  */
 
-public class Director {
+public class Director extends Manager {
+
+    private Double budget;
+
+    public Director(Double budget, String deptName, int id, String name, String socialSecurityNumber, double salary) {
+        super(deptName, id, name,socialSecurityNumber, salary);
+        this.budget = budget;
+    }
 }
